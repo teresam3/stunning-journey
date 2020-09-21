d788982c9ab6d73f3904a51d3a1bb357c9a84c1c
chilimango <maldonadot55@yahoo.com>
2020-09-20 21:05:10 -0700

    added link logos

--- work.js
+++ work.js
@@ -2,10 +2,13 @@
 
 function onScroll() {
 $(window).scroll(function() {
-        $("#nav-style .navbar-brand").addClass("white")
-        $("#nav-style").addClass("white")
-        $("a.nav-link").addClass("white")
-        });
+    $("#nav-style .navbar-brand").addClass("white")
+    $("#nav-style").addClass("white")
+    $("a.nav-link").addClass("white")
+    console.log("scroll works!")
+});
 }
 onScroll()
+
+
 
