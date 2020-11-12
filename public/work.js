daace10a0eac0d01b7b940adf13071d6ab410785
chilimango <maldonadot55@yahoo.com>
2020-11-11 20:53:31 -0800

    better navbar spacing and modal edits

--- public/work.js
+++ public/work.js
@@ -1,15 +1,14 @@
 //$(document).ready()
 
-//portflio hover
-function onHover() {
-$(window).onHover(function() {
-    $("#nav-style .navbar-brand").addClass("white")
-    $("#nav-style").addClass("white")
-    $("a.nav-link").addClass("white")
-    console.log("scroll works!")
-});
-}
-onScroll()
+// //portflio hover
+// function onHover() {
+// $(window).onHover(function() {
+//     $("#nav-style .navbar-brand").addClass("white")
+//     $("#nav-style").addClass("white")
+//     $("a.nav-link").addClass("white")
+//     console.log("scroll works!")
+// });
+// }
 
 
 
