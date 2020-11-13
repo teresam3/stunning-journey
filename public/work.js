d695d78b04977afde1e1154378c73f1b31de69d5
chilimango <maldonadot55@yahoo.com>
2020-11-12 21:47:54 -0800

    added onscroll js

--- public/work.js
+++ public/work.js
@@ -1,14 +1,29 @@
-//$(document).ready()
+$(document).ready()
 
-// //portflio hover
-// function onHover() {
-// $(window).onHover(function() {
-//     $("#nav-style .navbar-brand").addClass("white")
-//     $("#nav-style").addClass("white")
-//     $("a.nav-link").addClass("white")
-//     console.log("scroll works!")
-// });
-// }
+//portflio hover
+function landingHover() {
+    $(window).onHover(function() {
+        
+        console.log("scroll works!")
+    });
+}
 
 
+landingHover()
 
+
+var onScrollHandler = function() {
+    var newImageUrl = yourImageElement.src;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop > 100) {
+       newImageUrl = "img1.jpg"
+    }
+    if (scrollTop > 200) {
+       newImageUrl = "img2.jpg"
+    }
+    if (scrollTop > 300) {
+       newImageUrl = "img3.jpg"
+    }
+    yourImageElement.src = newImageUrl;
+  };
+  object.addEventListener ("scroll", onScrollHandler);
