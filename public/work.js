7793b44caf2f9e1040180f4e4b730655a05b68ec
chilimango <maldonadot55@yahoo.com>
2020-11-13 20:29:47 -0800

    finished about modal

--- public/work.js
+++ public/work.js
@@ -8,22 +8,21 @@ function landingHover() {
     });
 }
 
-
 landingHover()
 
 
-var onScrollHandler = function() {
-    var newImageUrl = yourImageElement.src;
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollTop > 100) {
-       newImageUrl = "img1.jpg"
-    }
-    if (scrollTop > 200) {
-       newImageUrl = "img2.jpg"
-    }
-    if (scrollTop > 300) {
-       newImageUrl = "img3.jpg"
-    }
-    yourImageElement.src = newImageUrl;
-  };
-  object.addEventListener ("scroll", onScrollHandler);
+// var onScrollHandler = function() {
+//     var newImageUrl = yourImageElement.src;
+//     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+//     if (scrollTop > 100) {
+//        newImageUrl = "img1.jpg"
+//     }
+//     if (scrollTop > 200) {
+//        newImageUrl = "img2.jpg"
+//     }
+//     if (scrollTop > 300) {
+//        newImageUrl = "img3.jpg"
+//     }
+//     yourImageElement.src = newImageUrl;
+//   };
+//   object.addEventListener ("scroll", onScrollHandler);
